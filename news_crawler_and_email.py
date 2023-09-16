84a8f23304b981da4bab02a852cd5c2541c2c549
Just2Deep <deepakrajsrs@gmail.com>
2023-09-16 11:36:19 +0530

    Updated to send mails

--- news_crawler_and_email.py
+++ news_crawler_and_email.py
@@ -1,17 +1,18 @@
 # Webscraping and automation
 
-import requests  # http requests
+# system date and time
+import datetime
+import os
 
-# Send the mail
+import requests  # http requests
 import smtplib
 from bs4 import BeautifulSoup  # web scraping
-
-# email body
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-# system date and time
-import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 now = datetime.datetime.now()
@@ -20,11 +21,10 @@ now = datetime.datetime.now()
 
 BASE_URL = "https://www.inshorts.com/en/read"
 CONTENT_LINK_URL = "https://inshorts.com"
-FROM = ""  # email address of the sender
-TO = ""  # email address of the receiver
-PASS = "-----------"  #
+FROM = os.environ["FROM"]  # email address of the sender
+TO = os.environ["TO"]  # email address of the receiver
+PASS = os.environ["PASS"]
 
-content = ""
 
 # Extracting News Stories
 
@@ -53,15 +53,13 @@ def extract_news(url: str) -> str:
     cnt += "<b>Inshorts Top Stories :</b>\n" + "<br>" + "-" * 80 + "<br><br>"
 
     soup = scrape_html(url)
-    article_title = fetch_required_data(
-        soup, element="span", attrs={"itemprop": "headline"}
-    )
-    article_body = fetch_required_data(
-        soup, element="div", attrs={"itemprop": "articleBody"}
-    )
-    article_link = fetch_required_data(
-        soup, element="span", attrs={"itemprop": "mainEntityOfPage"}
-    )
+    title_attrs = {"itemprop": "headline"}
+    body_attrs = {"itemprop": "articleBody"}
+    link_attrs = {"itemprop": "mainEntityOfPage"}
+
+    article_title = fetch_required_data(soup, element="span", attrs=title_attrs)
+    article_body = fetch_required_data(soup, element="div", attrs=body_attrs)
+    article_link = fetch_required_data(soup, element="span", attrs=link_attrs)
 
     for title, body, link in zip(article_title, article_body, article_link):
         cnt += (
@@ -96,13 +94,11 @@ def compose_html_for_mail(content: str) -> str:
     return content
 
 
-def send_the_mail(content):
+def send_the_mail(content: str):
     # Email Deatils
 
     SERVER = "smtp.gmail.com"  # your smtp server
     PORT = 587  # your port number
-
-    # password.(since the changes from google for less secure apps, we can use app password for this)
 
     # create text/plain message
     msg = MIMEText("")
@@ -122,7 +118,7 @@ def send_the_mail(content):
     print("Initiating Server....")
 
     server = smtplib.SMTP(SERVER, PORT)
-    server.set_debuglevel(1)  # to check errors
+    # server.set_debuglevel(1)  # to check messages and errors
     server.ehlo()
     server.starttls()
     server.login(FROM, PASS)
@@ -136,3 +132,4 @@ def send_the_mail(content):
 if __name__ == "__main__":
     content = extract_news(BASE_URL)
     final_content = compose_html_for_mail(content)
+    send_the_mail(final_content)
